8d9f2a05b367862b9cf414a1457353b687ca7023
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-10-05 17:43:59 +0530

    Added : ParamterizedTest for multiple email entries

--- src/test/java/io/junit/UserRegistrationTest.java
+++ src/test/java/io/junit/UserRegistrationTest.java
@@ -2,32 +2,33 @@ package io.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 
 class UserRegistrationTest {
 
-	@Test
-	public void givenEntry_whenAllDataIsValid_thenReturnHappyMood() {
-		UserRegistration obj = new UserRegistration();
-		String name = obj.validateInput("Bridgelabz");
-		String email = obj.validateEmail("abc@gmail.com");
-		String number = obj.validateNumber("91 7738454018");
-		String password = obj.validatePassword("Bridgelabz99@");		
-		assertTrue(name.equals("valid") && email.equals("valid") && number.equals("valid") && password.equals("valid"),"Sad");
-		System.out.println("Happy Mood");
+	private static UserRegistration userRegistration;
+	
+	@BeforeEach
+	void initiate() {
+		userRegistration = new UserRegistration();
 	}
 	
-	@Test
-	public void givenEntry_whenAnyDataIsInvalid_thenReturnSadMood() {
-		UserRegistration obj = new UserRegistration();
-		String name = obj.validateInput("Bridgelabz");
-		String email = obj.validateEmail("abc.xyz@bl.co.in");
-		String number = obj.validateNumber("91 7738454018");
-		String password = obj.validatePassword("Bridgelabz99@");
-		assertTrue(name.equals("invalid") || email.equals("invalid") || number.equals("invalid") || password.equals("invalid"),"Happy");
-		System.out.println("Sad Mood");
-	}
+	@ParameterizedTest
+	@ValueSource(strings = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au",
+			"abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"})
+	public void email_ifValid_shouldReturnValid(String validEmails) {
+		assertEquals("valid", userRegistration.validateEmail(validEmails));
 		
 	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*.com", "abc@%*.com", 
+			"abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"})
+	public void email_ifInvalid_shouldReturnInvalid(String invalidEmails) {
+		assertEquals("invalid", userRegistration.validateEmail(invalidEmails));
 		
-
+	}
+}	
